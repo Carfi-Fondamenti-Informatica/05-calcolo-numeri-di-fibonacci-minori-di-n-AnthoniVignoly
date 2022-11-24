29f45b3ae615c477778804a8fa5c3fd94a0c5565
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-11-24 23:33:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,8 +5,8 @@ int main() {
     int n=0, a=1, b=1, c=0;
     cin >> n;
     if (n>=2){
-        cout << a <<  b << endl;
-        for(int i=2; i<n; i++) {
+        cout << a << endl <<  b << endl;
+        for(int i=2; i>n; i++) {
             c=a+b;
             a=b;
             b=c;
