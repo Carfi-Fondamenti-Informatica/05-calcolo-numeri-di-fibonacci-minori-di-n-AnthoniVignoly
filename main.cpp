fde9b692209d66d888bb5111052cae804b9532ac
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-11-27 01:23:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,12 @@ using namespace std;
 int main() {
     int n=0, a=1, b=1, c=0;
     cin >> n;
-    if (n<=0){
         cout << a << endl <<  b << endl;
-        for(int i=0; i>=n; i--) {
+        for(int i=0; i<=n; i++) {
             c=a+b;
             a=b;
             b=c;
             cout << c << endl;
-        }
-    }
-    else {
-        cout <<"errore" << endl;
     }
    return 0;
 }
