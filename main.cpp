493067bbffa606989ba7a54d12767996be089ddd
AnthoniVignoly <115472326+AnthoniVignoly@users.noreply.github.com>
2022-11-27 01:11:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     cin >> n;
     if (n<=0){
         cout << a << endl <<  b << endl;
-        for(int i=0; i<=n; i--) {
+        for(int i=0; i>=n; i--) {
             c=a+b;
             a=b;
             b=c;
